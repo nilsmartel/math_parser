68272a6db343d691c22d3ebe57d0017ffacf3db8
Nils Martel <akantorabc@yahoo.de>
2019-06-11 17:58:58 +0200

    Cleaned imports

--- example/target/debug/deps/math_parser-901ffa67489dedd3.d
+++
@@ -1,5 +0,0 @@
-/Users/nilsmartel/Development/rust/math_parser/example/target/debug/deps/libmath_parser-901ffa67489dedd3.rlib: /Users/nilsmartel/Development/rust/math_parser/src/lib.rs
-
-/Users/nilsmartel/Development/rust/math_parser/example/target/debug/deps/math_parser-901ffa67489dedd3.d: /Users/nilsmartel/Development/rust/math_parser/src/lib.rs
-
-/Users/nilsmartel/Development/rust/math_parser/src/lib.rs:
--- example/target/debug/deps/version_check-4966fe6099262c54.d
+++
@@ -1,5 +0,0 @@
-/Users/nilsmartel/Development/rust/math_parser/example/target/debug/deps/libversion_check-4966fe6099262c54.rlib: /Users/nilsmartel/.cargo/registry/src/github.com-1ecc6299db9ec823/version_check-0.1.5/src/lib.rs
-
-/Users/nilsmartel/Development/rust/math_parser/example/target/debug/deps/version_check-4966fe6099262c54.d: /Users/nilsmartel/.cargo/registry/src/github.com-1ecc6299db9ec823/version_check-0.1.5/src/lib.rs
-
-/Users/nilsmartel/.cargo/registry/src/github.com-1ecc6299db9ec823/version_check-0.1.5/src/lib.rs:
